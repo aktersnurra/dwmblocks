3264d577dc0ad617e84a39152c13883a79c6d6df
Gustaf Rydholm <gustaf.rydholm@gmail.com>
2022-01-15 20:20:33 +0100

    Add gpu and temp info

--- config.h
+++ config.h
@@ -4,10 +4,12 @@ static const Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
     {"", "sb-pacpackages", 0, 8},
     {"", "sb-mailbox", 180, 12},
-    {"", "sb-cpubars", 10, 18},
-    {"", "sb-memory", 5, 4},
-    {"", "sb-nettraf", 1, 16},
+    {"󰢮 ", "sb-gpubar", 10, 18},
+    {"󰍛 ", "sb-cpubars", 10, 18},
+    {"", "sb-cpu-temp", 10, 18},
+    {"󰧑 ", "sb-memory", 5, 4},
     {"", "sb-internet", 5, 4},
+    {"", "sb-nettraf", 1, 16},
     {"", "sb-volume", 0, 10},
     {"", "sb-clock", 60, 1},
 };
